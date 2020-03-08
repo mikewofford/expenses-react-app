09eeddbb8318e7f87bd1a7c1e12290a61bb030a8
Mike Wofford <mikewofford07@yahoo.com>
2020-03-07 22:56:37 -0500

    Setup Prod Webpack Build

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,49 @@
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-module.exports = {
-    entry: "./src/app.js",
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/, //this tells babel to run js files
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/, //? allows for search of both scss and css. $ specifies ext
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map', //this is the source-map, this is dev, will add production version later,
-    devServer: { //Repl for live-server w/more dev features. live-server used in production
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true //This allows other routes to render
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css')
+    return {
+        entry: "./src/app.js",
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/, //this tells babel to run js files
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/, //? allows for search of both scss and css. $ specifies ext
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map', 
+        devServer: { //Repl for live-server w/more dev features. live-server used in production
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true //This allows other routes to render
+        }
     }
-}
+};
+
 
