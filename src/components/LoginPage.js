081781b721dd3d48ecfe95d3b119b4f8fb4e1072
Mike Wofford <mikewofford07@yahoo.com>
2020-03-15 23:04:58 -0400

    app complete

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,8 +5,12 @@ import { startLogin } from '../actions/auth';
 
 
 export const LoginPage = ({ startLogin }) => (
-    <div>
-        <button onClick={startLogin}>Login</button>
+    <div className="box-layout">
+        <div className="box-layout__box">
+            <h1 className="box-layout__title">Expenses App</h1>
+            <p>Track your expenses</p>
+        <button className="button" onClick={startLogin}>Login with Google</button>
+        </div>
     </div>
 )
 
